5a8a58d501b29a8c668ae67302ff5b0f230af47e
gandhy <gandhybong@gmail.com>
2020-06-05 13:59:00 +0700

    Revert "hapus folder database"
    This reverts commit b17f2731c47663c24517b0445f23d84494f501e0.

---
+++ database/migrations/2020_05_16_095742_create_table_band.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBand extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('band', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('nama_band');
+            $table->string('genre');
+            $table->string('skill_member');
+            $table->string('kota');
+            $table->string('logo')->default('/images/default-pp.jpg');
+            $table->text('deskripsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('band');
+    }
+}
